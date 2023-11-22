bd9976d0fedda0b18916d22b00d9bc9369f57644
AliiAhmadi <aliahmadi.github@gmail.com>
2023-11-22 20:54:13 +0330

    define basic token (add more later)

---
+++ token/token.go
@@ -0,0 +1,25 @@
+package token
+
+type TokenType string
+
+type Token struct {
+	Type    TokenType
+	Literal string
+}
+
+const (
+	ILLEGAL      = "ILLEGAL"
+	EOF          = "EOF"
+	ID           = "ID"
+	INT          = "INT"
+	ASSIGN       = "="
+	PLUS         = "+"
+	COMMA        = ","
+	SEMICOLON    = ";"
+	LPARENTHESES = "("
+	RPARENTHESES = ")"
+	LBRACE       = "{"
+	RBRACE       = "}"
+	FUNCTION     = "FUNCTION"
+	DEF          = "DEF"
+)
