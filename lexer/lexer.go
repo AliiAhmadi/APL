ace3ac6be9d71f30e793fc6a3da60b2df486ab28
AliiAhmadi <aliahmadi.github@gmail.com>
2023-11-23 15:42:55 +0330

    lexer 2

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -1,5 +1,7 @@
 package lexer
 
+import "Ahmadi/token"
+
 type Lexer struct {
 	input        string
 	position     int
@@ -8,9 +10,11 @@ type Lexer struct {
 }
 
 func New(input string) *Lexer {
-	return &Lexer{
+	lexer := &Lexer{
 		input: input,
 	}
+	lexer.readChar()
+	return lexer
 }
 
 func (l *Lexer) readChar() {
@@ -21,4 +25,39 @@ func (l *Lexer) readChar() {
 	}
 	l.position = l.readPosition
 	l.readPosition++
+}
+
+func (l *Lexer) NextToken() token.Token {
+	var tok token.Token
+
+	switch l.ch {
+	case '=':
+		tok = newToken(token.ASSIGN, l.ch)
+	case ';':
+		tok = newToken(token.SEMICOLON, l.ch)
+	case '(':
+		tok = newToken(token.LPARENTHESES, l.ch)
+	case ')':
+		tok = newToken(token.RPARENTHESES, l.ch)
+	case '}':
+		tok = newToken(token.RBRACE, l.ch)
+	case '{':
+		tok = newToken(token.LBRACE, l.ch)
+	case ',':
+		tok = newToken(token.COMMA, l.ch)
+	case '+':
+		tok = newToken(token.PLUS, l.ch)
+	case 0:
+		tok.Literal = ""
+		tok.Type = token.EOF
+	}
+	l.readChar()
+	return tok
+}
+
+func newToken(tokenType token.TokenType, ch byte) token.Token {
+	return token.Token{
+		Type:    tokenType,
+		Literal: string(ch),
+	}
 }
