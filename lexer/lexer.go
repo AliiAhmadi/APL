9ea9261bced87c60979224f2c23a5dd7cfb2d817
AliiAhmadi <aliahmadi.github@gmail.com>
2023-11-23 15:27:43 +0330

    working on lexer

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -6,3 +6,19 @@ type Lexer struct {
 	readPosition int
 	ch           byte
 }
+
+func New(input string) *Lexer {
+	return &Lexer{
+		input: input,
+	}
+}
+
+func (l *Lexer) readChar() {
+	if l.readPosition >= len(l.input) {
+		l.ch = 0
+	} else {
+		l.ch = l.input[l.readPosition]
+	}
+	l.position = l.readPosition
+	l.readPosition++
+}
--- lexer/lexer_test.go
+++ lexer/lexer_test.go
@@ -26,4 +26,16 @@ func TestNextToken(t *testing.T) {
 	}
 
 	l := New(input)
+
+	for i, test := range tests {
+		tok := l.NextToken()
+
+		if tok.Type != test.expectedType {
+			t.Fatalf("tests[%d] - tokentype wrong. expected=%q, got=%q", i, test.expectedType, tok.Type)
+		}
+
+		if tok.Literal != test.expectedLiteral {
+			t.Fatalf("tests[%d] - literal wrong. expected=%q, got=%q", i, test.expectedLiteral, tok.Literal)
+		}
+	}
 }
