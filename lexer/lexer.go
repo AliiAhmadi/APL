c90b7db2bbda66e12a54b6a3f33f9e5dd22089b5
AliiAhmadi <aliahmadi.github@gmail.com>
2023-11-23 17:17:43 +0330

    lexer #6 - skip white spaces

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -29,6 +29,7 @@ func (l *Lexer) readChar() {
 
 func (l *Lexer) NextToken() token.Token {
 	var tok token.Token
+	l.skipWhitespace()
 
 	switch l.ch {
 	case '=':
@@ -53,6 +54,7 @@ func (l *Lexer) NextToken() token.Token {
 	default:
 		if isLetter(l.ch) {
 			tok.Literal = l.readIndentifier()
+			tok.Type = token.LookupIdentifier(tok.Literal)
 			return tok
 		} else {
 			tok = newToken(token.ILLEGAL, l.ch)
@@ -79,4 +81,10 @@ func (l *Lexer) readIndentifier() string {
 		l.readChar()
 	}
 	return l.input[position:l.position]
+}
+
+func (l *Lexer) skipWhitespace() {
+	for l.ch == ' ' || l.ch == '\t' || l.ch == '\n' || l.ch == '\r' {
+		l.readChar()
+	}
 }
