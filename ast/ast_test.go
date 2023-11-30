0804dc166c54b2aa85ae7f8b8f88f0210569a672
AliiAhmadi <aliahmadi.github@gmail.com>
2023-11-30 20:21:03 +0330

    add another test. def statement and return statement

--- ast/ast_test.go
+++ ast/ast_test.go
@@ -35,3 +35,47 @@ func TestString(t *testing.T) {
 		t.Errorf("program.String() wrong. got='%q'", program.String())
 	}
 }
+
+func TestString2(t *testing.T) {
+	program := &Program{
+		Statements: []Statement{
+			&DefStatement{
+				Token: token.Token{
+					Type:    token.DEF,
+					Literal: "def",
+				},
+				Name: &Identifier{
+					Token: token.Token{
+						Type:    token.ID,
+						Literal: "variable",
+					},
+					Value: "variable",
+				},
+				Value: &Identifier{
+					Token: token.Token{
+						Type:    token.INT,
+						Literal: "t",
+					},
+					Value: "12",
+				},
+			},
+			&ReturnStatement{
+				Token: token.Token{
+					Type:    token.RETURN,
+					Literal: "return",
+				},
+				ReturnValue: &Identifier{
+					Token: token.Token{
+						Type:    token.ID,
+						Literal: "variable",
+					},
+					Value: "variable",
+				},
+			},
+		},
+	}
+
+	if program.String() != "def variable = 12;return variable;" {
+		t.Errorf("program.String() wrong. got=%q", program.String())
+	}
+}
