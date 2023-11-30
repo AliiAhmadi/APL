b2098ab698b9d51e45b6a572f66bba527f636779
AliiAhmadi <aliahmadi.github@gmail.com>
2023-11-30 19:48:42 +0330

    add String() for debugging

--- ast/ast.go
+++ ast/ast.go
@@ -2,10 +2,12 @@ package ast
 
 import (
 	"Ahmadi/token"
+	"bytes"
 )
 
 type Node interface {
 	TokenLiteral() string
+	String() string
 }
 
 type Statement interface {
@@ -30,6 +32,16 @@ func (p *Program) TokenLiteral() string {
 	}
 }
 
+func (p *Program) String() string {
+	var out bytes.Buffer
+
+	for _, statement := range p.Statements {
+		out.WriteString(statement.String())
+	}
+
+	return out.String()
+}
+
 type DefStatement struct {
 	Token token.Token // def
 	Name  *Identifier
@@ -38,6 +50,20 @@ type DefStatement struct {
 
 func (ds *DefStatement) statementNode()       {}
 func (ds *DefStatement) TokenLiteral() string { return ds.Token.Literal }
+func (ds *DefStatement) String() string {
+	var out bytes.Buffer
+
+	out.WriteString(ds.TokenLiteral() + " ")
+	out.WriteString(ds.Name.String())
+	out.WriteString(" = ")
+
+	if ds.Value != nil {
+		out.WriteString(ds.Value.String())
+	}
+
+	out.WriteString(";")
+	return out.String()
+}
 
 type Identifier struct {
 	Token token.Token
@@ -46,6 +72,9 @@ type Identifier struct {
 
 func (i *Identifier) expressionNode()      {}
 func (i *Identifier) TokenLiteral() string { return i.Token.Literal }
+func (i *Identifier) String() string {
+	return i.Value
+}
 
 type ReturnStatement struct {
 	Token       token.Token // return
@@ -54,6 +83,18 @@ type ReturnStatement struct {
 
 func (returnStatement *ReturnStatement) statementNode()       {}
 func (returnStatement *ReturnStatement) TokenLiteral() string { return returnStatement.Token.Literal }
+func (returnStatement *ReturnStatement) String() string {
+	var out bytes.Buffer
+
+	out.WriteString(returnStatement.TokenLiteral() + " ")
+
+	if returnStatement.ReturnValue != nil {
+		out.WriteString(returnStatement.ReturnValue.String())
+	}
+
+	out.WriteString(";")
+	return out.String()
+}
 
 type ExpressionStatement struct {
 	Token      token.Token
