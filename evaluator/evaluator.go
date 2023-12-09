612e335883eb31bbe6b9cd11f0791b0c101546cc
AliiAhmadi <aliahmadi.github@gmail.com>
2023-12-09 21:56:17 +0330

    bang operator evaluation

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -35,6 +35,11 @@ func Eval(node ast.Node) object.Object {
 	// Boolean
 	case *ast.Boolean:
 		return nativeBoolToBooleanObject(node.Value)
+
+	// Prefix Expression
+	case *ast.PrefixExpression:
+		right := Eval(node.Right)
+		return evalPrefixExpression(node.Operator, right)
 	}
 
 	return nil
@@ -55,4 +60,26 @@ func nativeBoolToBooleanObject(input bool) *object.Boolean {
 		return TRUE
 	}
 	return FALSE
+}
+
+func evalPrefixExpression(operator string, right object.Object) object.Object {
+	switch operator {
+	case "!":
+		return evalBangOperatorExpression(right)
+	default:
+		return nil
+	}
+}
+
+func evalBangOperatorExpression(right object.Object) object.Object {
+	switch right {
+	case TRUE:
+		return FALSE
+	case FALSE:
+		return TRUE
+	case NULL:
+		return TRUE
+	default:
+		return FALSE
+	}
 }
