a4492fef907e250b4241798faafa1e460f02479b
AliiAhmadi <aliahmadi.github@gmail.com>
2023-12-09 21:15:53 +0330

    tests completed

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -7,10 +7,30 @@ import (
 
 func Eval(node ast.Node) object.Object {
 	switch node := node.(type) {
+
+	// Statements
+	case *ast.Program:
+		return evalStatements(node.Statements)
+
+	// ExpressionStatement
+	case *ast.ExpressionStatement:
+		return Eval(node.Expression)
+
+	// Expressions
 	case *ast.IntegerLiteral:
 		return &object.Integer{
 			Value: node.Value,
 		}
 	}
 	return nil
+}
+
+func evalStatements(statements []ast.Statement) object.Object {
+	var result object.Object
+
+	for _, statement := range statements {
+		result = Eval(statement)
+	}
+
+	return result
 }
