55316409dc74d2b288429504e0dd1951b2141285
AliiAhmadi <aliahmadi.github@gmail.com>
2023-12-09 20:35:59 +0330

    working on evaluation

---
+++ object/object.go
@@ -0,0 +1,29 @@
+package object
+
+import "fmt"
+
+const (
+	INTEGER_OBJ = "INTEGER"
+	BOOLEAN_OBJ = "BOOLEAN"
+)
+
+type ObjectType string
+
+type Object interface {
+	Type() ObjectType
+	Inspect() string
+}
+
+type Integer struct {
+	Value int64
+}
+
+func (integer *Integer) Inspect() string  { return fmt.Sprintf("%d", integer.Value) }
+func (integer *Integer) Type() ObjectType { return INTEGER_OBJ }
+
+type Boolean struct {
+	Value bool
+}
+
+func (boolean *Boolean) Inspect() string  { return fmt.Sprintf("%t", boolean.Value) }
+func (boolean *Boolean) Type() ObjectType { return BOOLEAN_OBJ }
