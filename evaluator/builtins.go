7bef661c5f57b1233faa0c0977bd2aedf66e3e3b
AliiAhmadi <aliahmadi.github@gmail.com>
2023-12-11 21:46:56 +0330

    add builtin function for arrays

--- evaluator/builtins.go
+++ evaluator/builtins.go
@@ -15,9 +15,54 @@ var builtins = map[string]*object.Builtin{
 					Value: int64(len(arg.Value)),
 				}
 
+			case *object.Array:
+				return &object.Integer{
+					Value: int64(len(arg.Elements)),
+				}
+
 			default:
 				return newError("argument to `len` not supported, got %s", args[0].Type())
 			}
+		},
+	},
+
+	"first": &object.Builtin{
+		Fn: func(args ...object.Object) object.Object {
+			if len(args) != 1 {
+				return newError("wrong number of arguments. got=%d, want=1", len(args))
+			}
+
+			if args[0].Type() != object.ARRAY_OBJ {
+				return newError("argument to `first` must be ARRAY, got %s", args[0].Type())
+			}
+
+			arr := args[0].(*object.Array)
+			if len(arr.Elements) > 0 {
+				return arr.Elements[0]
+			}
+
+			return NULL
+		},
+	},
+
+	"last": &object.Builtin{
+		Fn: func(args ...object.Object) object.Object {
+			if len(args) != 1 {
+				return newError("wrong number of arguments. got=%d, want=1", len(args))
+			}
+
+			if args[0].Type() != object.ARRAY_OBJ {
+				return newError("argument to `last` must be ARRAY, got %s",
+					args[0].Type())
+			}
+
+			arr := args[0].(*object.Array)
+			length := len(arr.Elements)
+			if length > 0 {
+				return arr.Elements[length-1]
+			}
+
+			return NULL
 		},
 	},
 }
